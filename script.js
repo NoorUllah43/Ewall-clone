43c38e75d97e22b6d629c8175f63d121e18529b0
Noor Ullah <155673480+NoorUllah43@users.noreply.github.com>
2024-05-23 20:42:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function Products_alert(){
+    if(confirm("Do you want to buy this product")){
+        alert("The Porduct will be deliver to you in 3-days");
+    }
+    else{
+        alert("Thanks!")
+    }
+}
